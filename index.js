282f70533dd03d899c30b4f5767f34fa35714413
Dhananjay Kumar <>
2023-08-14 21:38:49 +0530

    Add aggregate

--- index.js
+++ index.js
@@ -24,6 +24,7 @@ import users from './router/user.js';
 import product from './router/product.js';
 import warehouse from './router/warehouse.js';
 import orders from './router/order.js';
+import aggregate from './router/aggregationService.js';
 
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -34,6 +35,7 @@ app.use('/users', users);
 app.use('/products', product);
 app.use('/warehouse', warehouse);
 app.use('/orders', orders);
+app.use('/aggregate', aggregate);
 
 app.listen(3002, function () {
      console.log(`listening on 3002`);
